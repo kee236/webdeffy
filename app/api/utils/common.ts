95f23cd2f769565a9bfda8b76a8dfabe85e4219d
kee236 <31869356+kee236@users.noreply.github.com>
2024-07-11 16:58:26 +0700

    Initialize feature/dockerfile

---
+++ app/api/utils/common.ts
@@ -0,0 +1,21 @@
+import { type NextRequest } from 'next/server'
+import { APP_ID, API_KEY } from '@/config'
+import { ChatClient } from 'langgenius-client'
+import { v4 } from 'uuid'
+
+const userPrefix = `user_${APP_ID}:`;
+
+export const getInfo = (request: NextRequest) => {
+  const sessionId = request.cookies.get('session_id')?.value || v4();
+  const user = userPrefix + sessionId;
+  return {
+    sessionId,
+    user
+  }
+}
+
+export const setSession = (sessionId: string) => {
+  return { 'Set-Cookie': `session_id=${sessionId}` }
+}
+
+export const client = new ChatClient(API_KEY)
